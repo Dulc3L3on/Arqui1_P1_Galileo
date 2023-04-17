c58fe261bcbaf83b97a2207d6ae2feb68bb15135
Dulc3L3on <ldulce2001@gmail.com>
2023-04-17 10:45:39 -0600

    se ha agregado la maquetacion del proyecto, se procedera a agregar la pantalla

---
+++ Maquetacion/rep_stepper_ultra_keypad.ino
@@ -0,0 +1,18 @@
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  myStepper.setSpeed(100);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // 1 rotation clockwise: 
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(2000);
+
+  // 1 rotation counterclockwise:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(2000);
+}
