a7d42ffdc9df1df678020c2a952f3db11739bf69
cresb-49 <carlosbpac@gmail.com>
2023-04-25 18:01:51 -0600

    implementacion sensores de deteccion y exportacion de resultado puerto serial

--- Arduino Code/code/code.ino
+++ Arduino Code/code/code.ino
@@ -1,6 +1,13 @@
 //libraries of modules
 #include <Keypad.h>
 #include <Stepper.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
+
+//Confiuraciones servo
+#define SERVO 26
+Servo servoMotor;
 
 //Params of dc motor
 #define STEPS 96
@@ -21,6 +28,18 @@ const double H2 = 20;
 const double H3 = 38;  //42.43
 double errorPercentage = 2.0;
 
+//Entradas de los sensores infrarojos
+#define REF1 22
+#define REF2 23
+#define REF3 24
+#define REF4 25
+
+//Confiuracion de pantalla LCD
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+//Array de tiempos
+unsigned long tiempos[] = { 0, 0, 0, 0 };
+double distancias[] = {0.16,0.18,0.16};
 
 //Matricial keyboar configs
 byte rows[4] = { 13, 12, 11, 10 };
@@ -43,17 +62,28 @@ void setup() {
   digitalWrite(Trigger, LOW);
   //Motor speed
   stepper.setSpeed(SPEED);
+  //Inicializacion de los pines de entrada leds infrarojos
+  pinMode(REF1, INPUT);
+  pinMode(REF2, INPUT);
+  pinMode(REF3, INPUT);
+  pinMode(REF4, INPUT);
+  //Init del servo
+  servoMotor.attach(SERVO);
+  servoMotor.write(180);//180 sujeta la pelota
+  // Inicializacion de la pantalla LCD
+  lcd.init();
+  lcd.backlight();
+  opcionesLCD();
 }
 
 void loop() {
   //Calcular la distancia de la plataforma
-  calcDistance();Serial.println(distance);
+  calcDistance();  //Serial.println(distance);
   //Obtener el valor del boton del teclado
   boton = teclado.getKey();
   //stepper.step(-12);
   switch (boton) {
     case '1':
-      Serial.println(distance);
       to15Degrees(((distance < H1) ? 1 : -1));
       boton = " ";
       break;
@@ -64,6 +94,12 @@ void loop() {
     case '3':
       to45Degrees(((distance < H3) ? 1 : -1));
       boton = " ";
+      break;
+    case '4':
+      servoMotor.write(150);//150 suelta la pelota
+      iniciarMedicion();
+      boton = " ";
+      servoMotor.write(180);//180 sujeta la pelota
       break;
   }
 }
@@ -105,7 +141,6 @@ void toDegrees(int dir, double high) {  //dir = 1/-1
       stepper.step(-12);
       calcDistance();
       Serial.println(distance);
-      
     }
   } else {
     //Sube la platarforma
@@ -115,7 +150,6 @@ void toDegrees(int dir, double high) {  //dir = 1/-1
       stepper.step(12);
       calcDistance();
       Serial.println(distance);
-      
     }
   }
 }
@@ -125,4 +159,73 @@ bool isWithinError(double val1, double val2, double errorPercentage) {
   double avg = (fabs(val1) + fabs(val2)) / 2.0;
   double error = avg * (errorPercentage / 100.0);
   return diff <= error;
+}
+
+void iniciarMedicion() {
+  lcd.clear();
+  lcd.print("Inicio Medicion");
+  int s1 = digitalRead(REF1);
+  int s2 = digitalRead(REF2);
+  int s3 = digitalRead(REF3);
+  int s4 = digitalRead(REF4);
+  bool bandera = true;
+  do {
+    s1 = digitalRead(REF1);
+    s2 = digitalRead(REF2);
+    s3 = digitalRead(REF3);
+    s4 = digitalRead(REF4);
+    //Si leemos 1 en el sensor 1 gurdamos el tiempo
+    if (s1 == 1) {
+      lcd.clear();
+      lcd.print("Sensor 1");
+      lcd.setCursor(0, 1);
+      lcd.print("Activado");
+      tiempos[0] = millis();
+    }
+    //Si leemos 1 en el sensor 2 gurdamos el tiempo
+    if (s2 == 1) {
+      lcd.clear();
+      lcd.print("Sensor 2");
+      lcd.setCursor(0, 1);
+      lcd.print("Activado");
+      tiempos[1] = millis();
+    }
+    //Si leemos 1 en el sensor 3 gurdamos el tiempo
+    if (s3 == 1) {
+      lcd.clear();
+      lcd.print("Sensor 3");
+      lcd.setCursor(0, 1);
+      lcd.print("Activado");
+      tiempos[2] = millis();
+    }
+    //Si leemos 1 en el sensor 4 gurdamos el tiempo
+    if (s4 == 1) {
+      lcd.clear();
+      lcd.print("Sensor 4");
+      lcd.setCursor(0, 1);
+      lcd.print("Activado");
+      tiempos[3] = millis();
+      //Detenemos el ciclo de ejecucion
+      bandera = false;
+    }
+  } while (bandera);
+  lcd.clear();
+  lcd.print("Fin Medicion");
+  String resultado =  String(tiempos[0])+","+String(tiempos[1])+","+String(tiempos[2])+","+String(tiempos[3]);
+  lcd.setCursor(0, 1);
+  lcd.print(resultado);
+  Serial.println(resultado);
+  delay(4000);
+  lcd.clear();
+  opcionesLCD();
+}
+
+void opcionesLCD(){
+  lcd.setCursor(0, 0);
+  lcd.print("Op: ");
+  lcd.print("1:15' ");
+  lcd.print("2:20' ");
+  lcd.setCursor(0, 1);
+  lcd.print("3:45' ");
+  lcd.print("4:init");
 }
